0109768faa4d5fc1f09d028d308ebe6fda706b0a
Saakib Minhaj <saakib148@gmail.com>
2026-01-14 16:40:07 +0530

    Adding Bean Programmatically Based on condition

---
+++ example07-AddingBeanProgrammatically/src/main/java/com/spring/bean/Vehicle.java
@@ -0,0 +1,29 @@
+package com.spring.bean;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void printHello(){
+        System.out.println("Printing hellllo msg from component annotaion pojo");
+    }
+
+    @Override
+    public String toString() {
+        return  name;
+    }
+}
