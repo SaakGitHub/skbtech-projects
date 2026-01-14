44a685ed0f838abacfc03883a0f424399becbf43
Saakib Minhaj <saakib148@gmail.com>
2026-01-14 15:03:38 +0530

    Component & Component scan StereoType Annotaion usage

---
+++ example05-ComponentAnnotation/src/main/java/com/spring/bean/Vehicle.java
@@ -0,0 +1,27 @@
+package com.spring.bean;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println("Printing hellllo msg from component annotaion pojo");
+    }
+
+    @Override
+    public String toString() {
+        return  name;
+    }
+}
---
+++ example05-ComponentAnnotation/src/main/java/com/spring/config/ProjectConfig.java
@@ -0,0 +1,13 @@
+package com.spring.config;
+
+import com.spring.bean.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "com.spring.bean")
+public class ProjectConfig {
+
+}
