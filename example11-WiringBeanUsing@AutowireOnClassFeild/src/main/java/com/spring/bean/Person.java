5c38d5f9eb8c643d048ab902f44305b878c45fbb
Saakib Minhaj <saakib148@gmail.com>
2026-01-16 16:51:16 +0530

    Wiring Bean using the @Autowired Annotation on class feild

---
+++ example11-WiringBeanUsing@AutowireOnClassFeild/src/main/java/com/spring/bean/Person.java
@@ -0,0 +1,40 @@
+package com.spring.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    public Person() {
+        System.out.println("Person Bean has bean created by SpringContext");
+    }
+
+    private String name = "polard";
+
+    @Autowired
+    private Vehicle vehicle;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", vehicle=" + vehicle +
+                '}';
+    }
+}
---
+++ example11-WiringBeanUsing@AutowireOnClassFeild/src/main/java/com/spring/config/ProjectConfig.java
@@ -0,0 +1,15 @@
+package com.spring.config;
+
+import com.spring.bean.Person;
+import com.spring.bean.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "com.spring.bean")
+public class ProjectConfig {
+
+
+}
